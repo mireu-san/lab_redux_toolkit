7d4fb617b5fd626bd3c470addf205921958c760f
Chesed Kim <starmireu@gmail.com>
2023-01-05 22:11:28 +0900

    removed unused import - app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
 import { createStore } from "redux";
 import { Provider, useSelector, useDispatch } from "react-redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 import store from "./store";
 import { up } from "./counterSlice";
-
-// function reducer(state, action) {
-//   if (action.type === "up") {
-//     return { ...state, value: state.value + action.step };
-//   }
-//   return state;
-// }
-// const initialState = { value: 0 };
-// const store = createStore(reducer, initialState);
 
 function Counter() {
   const dispatch = useDispatch();
