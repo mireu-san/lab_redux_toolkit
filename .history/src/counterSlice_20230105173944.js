e5c8619ccc32f64360b619d130bf5e9ced3e9720
Chesed Kim <starmireu@gmail.com>
2023-01-05 17:49:18 +0900

    removed history folder

--- .history/src/counterSlice_20230105173944.js
+++
@@ -1,9 +0,0 @@
-const counterSlice = createSlice({
-  name: "counterSlice",
-  initialState: { value: 0 },
-  reducers: {
-    up: (state, action) => {
-      state.value = state.value + action.payload;
-    },
-  },
-});
